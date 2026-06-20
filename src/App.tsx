0a9e39112add720955a881256422296098bc2539
Sathwika Thatiparthi <144071042+sathwika21n@users.noreply.github.com>
2026-06-20 17:36:18 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { Layout } from './components/Layout'
+import { ProgramPage } from './pages/ProgramPage'
+import { DanceDetailPage } from './pages/DanceDetailPage'
+import { QRPrintPage } from './pages/QRPrintPage'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/program" replace />} />
+          <Route path="/program" element={<ProgramPage />} />
+          <Route path="/program/:danceId" element={<DanceDetailPage />} />
+          <Route path="/qr" element={<QRPrintPage />} />
+          <Route path="*" element={<Navigate to="/program" replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
